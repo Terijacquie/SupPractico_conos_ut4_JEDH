d6b123aff79ecea655d66ca47ae0cccb6bd48663
Jacqueline E. Díaz Hadjar <terijacquie@gmail.com>
2026-04-24 09:41:32 +0100

    agregadas nuevas funciones

---
+++ src/Cono.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+/**
+ * Clase Cono
+ */
+public class Cono  {
+
+    private float base;
+    private float altura;
+
+    public Cono (float base, float altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    /**
+     * Metodo para calcular el volumen del cono
+     * @return
+     */
+    public double calculoVolumen() {
+        float radio = Float.parseFloat(JOptionPane.showInputDialog(null,
+                "Ingrese el radio del cono"));
+        double vol = (3.14) * (Math.pow(radio, 2)) * getAltura() / 3;
+        return vol;
+    }
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Cono{" +
+                "altura=" + altura +
+                ", base=" + base +
+                '}';
+    }
+}
+
+
--- src/MainCono.java
+++ src/MainCono.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainCono {
+public static ArrayList<Cono> conos = new ArrayList <Cono>();
+
+    public static void main(String[] args) {
+
+        crearConos();
+        imprimirVolumen();
+
+    }
+
+    /**
+     * Metodo para crear los conos y añadir al arraylist
+     */
+    public static void crearConos() {
+        int cantidad = 4;
+        for (int i = 0; i < cantidad; i++) {
+            double altura = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la altura del cono "+(i+1)));
+            double base = Double.parseDouble(JOptionPane.showInputDialog
+                    (null, "Ingrese la medida de la base del cono " + (i + 1)));
+
+            Cono miCubo= new Cono((float) base,(float) altura);
+            conos.add(miCubo);
+        }
+    }
+
+    /**
+     * Método para imprimir el volúmen de los conos
+     * @param
+     */
+    public static void imprimirVolumen() {
+        for (Cono c : conos) {
+            JOptionPane.showMessageDialog(null,
+                    "El volúmen es de " + c.calculoVolumen());
+        }
+    }
+
+
 }
